430b32d877ec087dfc297bb5d1565a4c4f628269
Jhilmar <el.jhilmar@hotmail.com>
2015-12-10 16:32:22 -0400

    primer commit

---
+++ js/principal.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$("h1").addClass("animated rubberBand");
+	$("small").addClass("animated fadeInLeftBig");
+	$("button:over").addClass("animated rubberBand")
+	//$("#target3").addClass("animated fadeOut");
+
+	// $("button").addClass("animated");
+ //    $(".btn").addClass("shake");
+	// $("#target1").addClass("btn-primary");
+	
+	// $("button").removeClass("btn-default");
+
+	// $("#target1").css("color","red");
+	
+	// $("#target1").prop("disabled", true);
+	
+	// $("#target4").html("<i>#target4</i>");
+	
+	// $("#target4").remove();
+
+	// $("#target2").appendTo("#right-well");
+
+	// $("#target5").clone().appendTo("#left-well");
+
+	// $("#target1").parent().css("background-color","red");
+
+ //    $("#right-well").children().css("color","green");
+
+	// $(".target:nth-child(2)").addClass("animated bounce");
+
+	// $(".target:even").addClass("animated shake");
+
+	//$("body").addClass("animated hinge");
+
+
+});
